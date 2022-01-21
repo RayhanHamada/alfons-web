a765772da62858c3a936be6ef61ca53d924506eb
RayhanHamada <rayhanhamada1999@gmail.com>
2022-01-22 00:30:24 +0700

    refactor: supabase to supabaseClient

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,4 +1,4 @@
-import { definitions } from '@/types/supabase';
+import type { definitions } from '@/types/supabase';
 import type { SupabaseClient } from '@bnjmnt4n/supabase-client';
 import { createClient } from '@supabase/supabase-js';
 
@@ -10,3 +10,5 @@ const supabaseClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 ) as unknown as SupabaseClient<Defs>;
+
+export default supabaseClient;
