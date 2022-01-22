ae6242bcc13e99046024dd1b3499b9ff3cea0dcd
RayhanHamada <rayhanhamada1999@gmail.com>
2022-01-22 21:43:22 +0700

    feat: add onClick routing to createAppointment

--- src/components/home/CreateAppointmentButton.tsx
+++ src/components/home/CreateAppointmentButton.tsx
@@ -1,8 +1,16 @@
 import { Button } from 'antd';
+import { useRouter } from 'next/router';
+import type { MouseEventHandler } from 'react';
 
 export const CreateAppointmentButton: React.FC = (_props) => {
+  const router = useRouter();
+
+  const onClick: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    await router.push('/createAppointment');
+  };
+
   return (
-    <Button type="primary" size="large">
+    <Button type="primary" size="large" onClick={onClick}>
       Make an Appointment
     </Button>
   );
